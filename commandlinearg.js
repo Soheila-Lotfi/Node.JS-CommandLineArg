9ab1cd6e14c884249ed7ec662b44bb02d2409031
soheila <soheilalotfi@ymail.com>
2019-07-22 09:37:31 -0400

    take in two command line arguments and check if they are equal

---
+++ commandlinearg.js
@@ -0,0 +1,15 @@
+var argOne=process.argv[2];
+var argTwo=process.argv[3];
+
+if( argOne === argTwo){
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+if( argOne%7 === 0 && argTwo%7 === 0){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
